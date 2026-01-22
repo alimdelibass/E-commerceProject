ab44086d03c17a7dca85b4a167f52a8dc0fc3e2b
alimdelibass <alim.delibass@gmail.com>
2026-01-22 23:12:48 +0300

    ALTYAPI-2

---
+++ ETicaretAPI/Infrastructure/ETicaretAPI.Persistence/Repositories/WriteRepository.cs
@@ -0,0 +1,40 @@
+﻿using ETicaretAPI.Application.Repositories;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ETicaretAPI.Persistence.Repositories
+{
+    public class WriteRepository<T> : IWriteRepository<T> where T : class
+    {
+        public DbSet<T> Table => throw new NotImplementedException();
+
+        public Task<bool> AddAsync(T model)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<bool> AddAsync(List<T> model)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<bool> Remove(T model)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<bool> RemoveAsync(Guid id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<bool> UpdateAsync(T model)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
