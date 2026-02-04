591ce919478c7cbb94c0f60ac35efe4be0e7f019
alimdelibass <alim.delibass@gmail.com>
2026-02-04 22:37:36 +0300

    ALTYAPI-3

--- ETicaretClient/src/app/admin/layout/layout.ts
+++ ETicaretClient/src/app/admin/layout/layout.ts
@@ -3,12 +3,12 @@ import { RouterOutlet } from '@angular/router';
 import { Header } from './components/header/header';
 import { Sidebar } from "./components/sidebar/sidebar";
 import { Footer } from './components/footer/footer';
-import { MatSidenavModule, MatDrawer, MatDrawerContainer, MatDrawerContent } from '@angular/material/sidenav';
+import { MatDrawer, MatDrawerContainer, MatDrawerContent } from '@angular/material/sidenav';
 
 @Component({
   selector: 'app-layout',
   standalone: true,
-  imports: [Header, RouterOutlet, Sidebar, Footer, MatDrawer, MatDrawerContainer, MatDrawerContent],
+  imports: [RouterOutlet, Header, Sidebar, Footer, MatDrawer, MatDrawerContainer, MatDrawerContent],
   templateUrl: './layout.html',
   styleUrl: './layout.scss'
 })
