7f86175887dce82d55306a3585a487c2408fbe94
alimdelibass <alim.delibass@gmail.com>
2026-03-30 20:51:55 +0300

    ALTYAPI-15

---
+++ ETicaretAPI/Core/ETicaretAPI.Application/Features/Queries/GetAllProduct/GetAllProductQueryHandler.cs
@@ -0,0 +1,18 @@
+﻿using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ETicaretAPI.Application.Features.Queries.GetAllProduct
+{
+    public class GetAllProductQueryHandler : IRequestHandler<GetAllProductQueryRequest, GetAllProductQueryResponse>
+    {
+        public GetAllProductQueryHandler() { }
+        public Task<GetAllProductQueryResponse> Handle(GetAllProductQueryRequest request, CancellationToken cancellationToken)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
